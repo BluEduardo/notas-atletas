b5ae176be9be519039d9b15c7beafe859d1bafd8
BluEduardo <eduardorobertopereira604@gmail.com>
2025-10-06 11:15:33 -0300

    first commit

---
+++ notas-atletas.js
@@ -0,0 +1,33 @@
+let atletas = [
+    {
+      nome: "Cesar Abascal",
+      notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+      nome: "Fernando Puntel",
+      notas:  [8, 10, 10, 7, 9.33]
+    },
+    {
+      nome: "Daiane Jelinsky",
+      notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+      nome: "Bruno Castro",
+      notas: [10, 10, 10, 9, 9.5]
+    }
+   ];
+
+   atletas.forEach(function(atleta){
+
+        notasOrdenadas = atleta.notas.sort((a,b) => a-b);
+        notasComputadas = notasOrdenadas.slice(1, 4);
+        notasSomadas = notasComputadas.reduce((total, nota) => total+nota, 0);
+        media = notasSomadas/notasComputadas.length;
+
+        console.log("\n");
+        console.log(atleta.nome);
+        console.log(`Notas Obtidas: ${notasOrdenadas}`);
+        console.log(`Média Válida: ${media}`);
+
+   });
+
